38319defe65b605d9eabfb7da514536e87b21b22
qwer12211 <145342155+qwer12211@users.noreply.github.com>
2023-10-10 19:42:43 +0300

    Update Program.cs

--- Pianinko/Program.cs
+++ Pianinko/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Threading;
-
 class Piano
 {
-    static int[] fourdoctave = new int[] { 261,277, 293, 311, 294, 329, 349, 370, 392, 415, 440, 466, 493 }; // Частоты для четвёртой октавы (C4-B4) 
+    static int[] fourdoctave = new int[] { 261,277, 293, 311, 329, 349, 370, 392, 415, 440, 466, 493 }; // Частоты для четвёртой октавы (C4-B4) 
     static int[] fithoctave = new int[] { 523, 554, 587, 622, 659, 698, 740, 784, 830, 880, 932, 987 }; // Частоты для пятой  октавы (C5-B5) 
     static int[] sixthoctave = new int[] { 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976 };  // Частоты для шестой октавы (C6-B6)
     static int[] nashatoctave; // Текущая октава 
     static bool running = true;
-
     static void Main(string[] args)
     {
         Console.WriteLine("Приветсвую вас в консольном пианино.");
@@ -48,7 +46,6 @@ class Piano
             }
         }
     }
-
     static void PlaySound(ConsoleKey key)
     {
         int index = -1;
@@ -91,8 +88,10 @@ class Piano
             case ConsoleKey.I:
                 index = 11;
                 break ;
+            case ConsoleKey.O:
+                index = 12;
+                break ;
         }
-
         if (index >= 0)
         {
             int ZXC = nashatoctave[index];
